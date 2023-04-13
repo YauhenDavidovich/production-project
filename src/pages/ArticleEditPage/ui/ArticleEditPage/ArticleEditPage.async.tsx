ff575e6b22245ee8f2b7670dac0a55e2b889307b
Yauhen Davidovich <davidovich336@gmail.com>
2023-04-13 15:06:56 +0200

    feat: production

--- src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.async.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-export const ArticleEditPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // ONLY IN TEST PROJECTS
-    setTimeout(() => resolve(import('./ArticleEditPage')), 1000);
-}));
+export const ArticleEditPageAsync = lazy(() => import('./ArticleEditPage'));
--- src/pages/ProfilePage/ui/ProfilePage.async.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-export const ProfilePageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // ONLY IN TEST PROJECTS
-    setTimeout(() => resolve(import('./ProfilePage')), 1000);
-}));
+export const ProfilePageAsync = lazy(() => import('./ProfilePage'));
